df860056deed55b05202c5e2c6e7cc39b544f85b
Ilya Maximov <mail@overlisted.net>
2020-12-11 19:51:05 +0100

    write the logic

--- src/main/kotlin/net/overlisted/shitposter/main.kt
+++ src/main/kotlin/net/overlisted/shitposter/main.kt
@@ -1,17 +1,49 @@
 package net.overlisted.shitposter
 
+import com.jessecorbett.diskord.api.rest.client.ChannelClient
 import com.jessecorbett.diskord.dsl.bot
 import com.jessecorbett.diskord.dsl.command
 import com.jessecorbett.diskord.dsl.commands
-import java.lang.Exception
+import com.jessecorbett.diskord.util.sendMessage
+
+fun readResource(filename: String) = Shitposter::class.java.classLoader.getResource(filename)?.readText()
 
 class Shitposter {
+  private val adjectives: List<String>
+  private val nouns: List<String>
 
+  init {
+    val adjectivesTxt = readResource("english-adjectives.txt")
+    val nounsTxt = readResource("english-nouns.txt")
+
+    if(adjectivesTxt != null && nounsTxt != null) {
+      adjectives = adjectivesTxt.split("\n")
+      nouns = nounsTxt.split("\n")
+    } else throw Exception("Can't load the dictionaries!")
+  }
+
+  suspend fun doShit(channel: ChannelClient) {
+    val adj = adjectives[(Math.random() * adjectives.size).toInt()]
+    val noun = nouns[(Math.random() * nouns.size).toInt()]
+
+    channel.sendMessage("$adj $noun.")
+  }
 }
 
 suspend fun main() {
-  bot("") {
+  val token = readResource("token.txt") ?: return
+
+  bot(token) {
     val sp = Shitposter()
+
+    var lastTime = System.currentTimeMillis()
+    messageCreated {
+      if(System.currentTimeMillis() - 1000 > lastTime) {
+        sp.doShit(it.channel)
+
+        lastTime = System.currentTimeMillis()
+      }
+    }
 
     commands {
       command("sp interval") {
